deae6bf5e4b9ba891ba7d634473d068be13cf580
Charles Zambrano <charleszambranoespe@outlook.com>
2024-08-05 12:45:03 -0500

    [ADD] CORS

--- invoicedoc/src/main/java/com/banquito/core/invoicedoc/controller/InvoiceController.java
+++ invoicedoc/src/main/java/com/banquito/core/invoicedoc/controller/InvoiceController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/invoice-microservice/api/v1/invoices")
 @Validated
 @Slf4j
+@CrossOrigin(origins = "*")
 @Tag(name = "Invoice", description = "Endpoints for managing invoices")
 public class InvoiceController {
 
--- invoicedoc/src/main/java/com/banquito/core/invoicedoc/controller/TaxController.java
+++ invoicedoc/src/main/java/com/banquito/core/invoicedoc/controller/TaxController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/invoice-microservice/api/v1/taxes")
 @Validated
 @Slf4j
+@CrossOrigin(origins = "*")
 @Tag(name = "Tax", description = "Endpoints for managing taxes")
 public class TaxController {
 
